60b6b78f5c26589668704198b071b8015e7464fa
Gabriel <gh.barreto@hotmail.com>
2023-05-24 19:24:54 -0700

    first steps

---
+++ HelloWorld/src/FirstClass.java
@@ -0,0 +1,15 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 5;
+        boolean isAlien = false;
+
+        if(x > 3){
+            System.out.println("bigger");
+        }
+
+        if(!isAlien){
+             System.out.println("not alien");
+        }
+    }
+}
